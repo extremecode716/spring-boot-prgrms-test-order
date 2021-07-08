1c74868323b58f95c4bcef1a5fba5afc759c858b
ExtremeCode <extremecode716@gmail.com>
2021-07-08 16:25:00 +0900

    [Sol] 요건1. API 응답 포맷 해결

--- src/main/java/com/github/prgrms/products/ProductRestController.java
+++ src/main/java/com/github/prgrms/products/ProductRestController.java
@@ -1,6 +1,8 @@
 package com.github.prgrms.products;
 
 import com.github.prgrms.errors.NotFoundException;
+
+import com.github.prgrms.utils.ApiUtils.ApiResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static com.github.prgrms.utils.ApiUtils.success;
 import static java.util.stream.Collectors.toList;
 
 @RestController
@@ -20,20 +23,20 @@ public class ProductRestController {
         this.productService = productService;
     }
 
-    // FIXME `요건 1` 정의에 맞게 응답 타입 수정이 필요합니다.
+    // FIXME `요건 1` 정의에 맞게 응답 타입 수정이 필요합니다. (완료)
     @GetMapping(path = "{id}")
-    public ProductDto findById(@PathVariable Long id) {
-        return productService.findById(id)
+    public ApiResult<ProductDto> findById(@PathVariable Long id) {
+        return success(productService.findById(id)
                 .map(ProductDto::new)
-                .orElseThrow(() -> new NotFoundException("Could not found product for " + id));
+                .orElseThrow(() -> new NotFoundException("Could not found product for " + id)));
     }
 
-    // FIXME `요건 1` 정의에 맞게 응답 타입 수정이 필요합니다.
+    // FIXME `요건 1` 정의에 맞게 응답 타입 수정이 필요합니다. (완료)
     @GetMapping
-    public List<ProductDto> findAll() {
-        return productService.findAll().stream()
+    public ApiResult<List<ProductDto>> findAll() {
+        return success(productService.findAll().stream()
                 .map(ProductDto::new)
-                .collect(toList());
+                .collect(toList()));
     }
 
 }
